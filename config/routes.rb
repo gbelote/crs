bc5b6fa33bed3bf170a995b8019062f679589685
Greg Belote <etoleb@gmail.com>
2009-08-23 00:51:52 +0000

    Revert ".."
    This reverts commit f9b9b9d4db6f88f13b0214296bf51f0b10095594.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.login "login", :controller => :user_sessions, :action => :new
-  map.logout "logout", :controller => :user_sessions, :action => :destroy
-
   map.resources :users
-  map.resources :user_sessions
 
   # The priority is based upon order of creation: first created -> highest priority.
 
